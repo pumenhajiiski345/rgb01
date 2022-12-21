a2c7168b0347001012dc26c285ea5582f347b795
pumenhajiiski345 <121171160+pumenhajiiski345@users.noreply.github.com>
2022-12-21 14:25:16 +0200

    Първоначални файлове за MakeCode проекта

---
+++ main.ts
@@ -0,0 +1,27 @@
+let B = 0
+let G = 0
+let R = 0
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+basic.forever(function () {
+    R = 0
+    G = 0
+    G = 0
+    for (let index = 0; index < 255; index++) {
+        R += 1
+        B += -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1)
+    }
+    for (let index = 0; index < 255; index++) {
+        G += 1
+        R += -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1)
+    }
+    for (let index = 0; index < 255; index++) {
+        B += 1
+        G += -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1)
+    }
+})
